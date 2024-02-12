2b96dc0b068d7ac383efd31acca331fa4a765945
mrbrandonarnold <brandonarnold926@gmail.com>
2024-02-12 15:27:48 -0500

    Cleaned openshift resources

--- download_certificates.sh
+++ download_certificates.sh
@@ -14,13 +14,13 @@ function catch {
 # Run your commands within the try-catch block
 
 # Wget commands
-# try wget --no-check-certificate -r -np -nd -R "index.html*" https://wcf-serve.apps.arena-workspace.navair.navy.mil/wcf/latest/crt/ -P /usr/local/share/ca-certificates/WCF || catch "wget command failed"
-# try wget --no-check-certificate -r -np -nd -R "index.html*" https://wcf-serve.apps.arena-workspace.navair.navy.mil/dod/latest/ -P /usr/local/share/ca-certificates/WCF || catch "wget command failed"
+# try wget --no-check-certificate -r -np -nd -R "index.html*" /wcf/latest/crt/ -P /usr/local/share/ca-certificates/WCF || catch "wget command failed"
+# try wget --no-check-certificate -r -np -nd -R "index.html*" /dod/latest/ -P /usr/local/share/ca-certificates/WCF || catch "wget command failed"
 
 # Curl and unzip commands
-try curl -L -o /usr/local/share/ca-certificates/WCF/crt.zip https://wcf-serve.apps.arena-workspace.navair.navy.mil/wcf/latest/crt.zip || catch "curl command failed"
+try curl -L -o /usr/local/share/ca-certificates/WCF/crt.zip /wcf/latest/crt.zip || catch "curl command failed"
 try unzip -o /usr/local/share/ca-certificates/WCF/crt.zip -d /usr/local/share/ca-certificates/WCF || catch "unzip command failed"
-try curl -L -o /usr/local/share/ca-certificates/WCF/dod.zip https://wcf-serve.apps.arena-workspace.navair.navy.mil/dod/latest/dod.zip || catch "curl command failed"
+try curl -L -o /usr/local/share/ca-certificates/WCF/dod.zip /dod/latest/dod.zip || catch "curl command failed"
 try unzip -o /usr/local/share/ca-certificates/WCF/dod.zip -d /usr/local/share/ca-certificates/WCF || catch "unzip failed"
 
 echo "Commands executed successfully"
